9bc53127cbe21776b2242788246e78de944846d2
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-05-15 02:28:31 +0530

    read bills customer ID method

--- src/main/java/model/Bill.java
+++ src/main/java/model/Bill.java
@@ -132,4 +132,80 @@ public class Bill {
             return "{\"status\":\"error\", \"code\":500, \"data\":\"Error while reading bills by connection ID\"}";
         }
     }
+    
+    // read bills by customerID
+    public String readCustomerBills(String customerID) {
+        String table = "";
+            
+        try {
+            Connection connection = DBConnection.connect();
+
+            if (connection == null) {
+                return "{\"status\":\"error\", \"code\":500, \"data\":\"Error while connecting database for reading bills by customer ID.\"}";
+            }
+
+            // Prepare the html table to be displayed
+			table = "<table>" 
+                    + "<tr>"
+                        + "<th>Connection ID</th>" + "<th>Customer ID</th>"
+                        + "<th>Customer Name</th>" + "<th>Issued Date</th>" + "<th>Due Date</th>"
+                        + "<th>Units</th>" + "<th>Amount</th>" + "<th>Payment Status</th>"
+                        + "<th>Update Payment</th>" + "<th>Delete Bill</th>"
+                    + "</tr>";
+
+            // sql statement to retrieve bills by connection ID
+            String sql =    "SELECT * " +
+                            "FROM Bill B, Connection C, Customer U " + 
+                            "WHERE B.connectionID = C.connectionID and C.customerID = U.customerID and U.customerID = ?";
+
+            // binding connectionID and executing the query
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, Integer.parseInt(customerID));
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            BillBean billBean = new BillBean();
+
+            // looping through the rows
+            while (resultSet.next()) {
+                billBean.setBillID(resultSet.getInt("billID"));
+                billBean.setConnectionID(resultSet.getInt("connectionID"));
+                billBean.setCustomerID(resultSet.getInt("customerID"));
+                billBean.setCustomerName(resultSet.getString("firstname") + " " + resultSet.getString("lastname"));
+                billBean.setPaymentID(resultSet.getInt("paymentID"));
+                billBean.setIssuedDate(resultSet.getString("issueDate"));
+                billBean.setDueDate(resultSet.getString("dueDate"));
+                billBean.setStatus(resultSet.getString("status"));
+                billBean.setUnits(resultSet.getInt("units"));
+                billBean.setAmount(resultSet.getDouble("amount"));
+
+                // add the data to the html table
+                table += "<tr><td>" + billBean.getConnectionID() + "</td>";
+                table += "<td>" + billBean.getCustomerID() + "</td>";
+                table += "<td>" + billBean.getCustomerName() + "</td>";
+                table += "<td>" + billBean.getIssuedDate() + "</td>";
+                table += "<td>" + billBean.getDueDate() + "</td>";
+                table += "<td>" + billBean.getUnits() + "</td>";
+                table += "<td>" + billBean.getAmount() + "</td>";
+                table += "<td>" + billBean.getStatus() + "</td>";
+                
+                //buttons
+        		table += "<td>";
+        		table += "<input name='btnUpdate' type='button' value='Update Payment' class='btnUpdate btn btn-sm btn-secondary' data-billid='" + billBean.getBillID() + "'>";
+        		table += "</td>"; 
+        		table += "<td>";
+        		table += "<input name='btnRemove' type='button' value='Remove' class='btn btn-sm btn-danger btnRemove' data-billid='" + billBean.getBillID() + "'>";
+        		table += "</td></tr>";
+            }
+            table += "</table>";
+            
+            connection.close();
+
+            return "{\"status\":\"success\", \"code\":200, \"data\":\"" + table + "\"}";
+            
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return "{\"status\":\"error\", \"code\":500, \"data\":\"Error while reading bills by customer ID\"}";
+        }
+    }
+
 }
